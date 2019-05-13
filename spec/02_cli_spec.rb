7f1f7fea87ea8ce7938ad42f41eecbcced1fbfbf
Kabalan Elahmadie <kahmadieh@yahoo.com>
2019-05-13 05:03:52 +0000

    Automatically backed up by Learn

--- spec/02_cli_spec.rb
+++ spec/02_cli_spec.rb
@@ -1,14 +1,6 @@
 require_relative "../lib/greeting.rb"
 
 describe './bin/greet executing a CLI Application' do
-  it 'outputs a welcome message and asks the user for input' do
-    allow($stdout).to receive(:puts)
-    allow(self).to receive(:gets).and_return("Don")
-
-    expect($stdout).to receive(:puts).with("Hi! I'm HAL, what's your name?"), "Make sure bin/greet has code that can output 'Hi! I'm HAL, what's your name?' exactly."
-
-    run_file("./bin/greet")
-  end
 
   it 'uses #gets.strip to capture the user input and set it equal to a variable called name' do 
     allow($stdout).to receive(:puts)
